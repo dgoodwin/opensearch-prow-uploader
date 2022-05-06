31a825f621ed1deee789c5fbc0e944836a0bd40b
Devan Goodwin <dgoodwin@redhat.com>
2022-05-06 08:06:32 -0300

    User/pass cli args.

--- cmd/uploadProwJob.go
+++ cmd/uploadProwJob.go
@@ -1,9 +1,20 @@
 package cmd
 
 import (
+	"crypto/tls"
+	"net/http"
+
+	"github.com/opensearch-project/opensearch-go"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
+
+type UploadOpts struct {
+	Username string
+	Password string
+}
+
+var opts UploadOpts
 
 // uploadProwJobCmd represents the uploadProwJob command
 var uploadProwJobCmd = &cobra.Command{
@@ -15,7 +26,25 @@ var uploadProwJobCmd = &cobra.Command{
 		}
 		prowJobURL := args[0]
 		log.WithField("prowJob", prowJobURL).Info("uploading prow job")
+		run()
 	},
+}
+
+func run() {
+	client, err := opensearch.NewClient(opensearch.Config{
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+		Addresses: []string{"https://search-dgoodwin-test-o4g3tsj6smjnfyxybu4m67ospy.us-east-1.es.amazonaws.com"},
+		Username:  opts.Username, // For testing only. Don't store credentials in code.
+		Password:  opts.Password,
+	})
+	if err != nil {
+		log.WithError(err).Fatal("error connecting to opensearch")
+	}
+
+	// Print OpenSearch version information on console.
+	log.Info(client.Info())
 }
 
 func init() {
@@ -25,9 +54,10 @@ func init() {
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// uploadProwJobCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// uploadProwJobCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	uploadProwJobCmd.Flags().StringVar(&opts.Username, "user", "openshift", "TRT OpenSearch username")
+	uploadProwJobCmd.Flags().StringVar(&opts.Password, "pass", "", "TRT OpenSearch password")
 }
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mitchellh/mapstructure v1.4.3 // indirect
+	github.com/opensearch-project/opensearch-go v1.1.0 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.0-beta.8 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
